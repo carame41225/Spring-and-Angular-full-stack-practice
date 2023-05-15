5dcbc28ff2492ed2e65a74053de2cbadf38163f8
kai <carame311005@gmail.com>
2023-05-15 17:36:43 +0800

    htps

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default{
     oidc:{
         clientId:'0oa9fg7xy26yViYoy5d7',
         issuer:'https://dev-15566460.okta.com/oauth2/default',
-        redirectUri:'http://localhost:4200/login/callback',
+        redirectUri:'https://localhost:4200/login/callback',
         scopes:['openid','profile','email']
     }
 }
